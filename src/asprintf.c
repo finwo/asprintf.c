4e144f4f788c28908776eed85a1752225163f051
finwo <finwo@pm.me>
2023-10-25 22:59:12 +0200

    Actually make it properly alloc

--- src/asprintf.c
+++ src/asprintf.c
@@ -4,13 +4,13 @@
 #include <stdlib.h>
 
 // Polyfill asprintf, comes from stdio when _GNU_SOURCE is defined
-char * asprintf(char **restrict strp, const char *restrict fmt, ...) {
+int asprintf(char **restrict strp, const char *restrict fmt, ...) {
   va_list argptr;
   va_start(argptr, fmt);
   int size = vsnprintf(NULL, 0, fmt, argptr);
-  char *output = calloc(1, size + 1);
-  vsnprintf(output, size + 1, fmt, argptr);
+  *strp = realloc(*strp, size + 1);
+  vsnprintf(*strp, size + 1, fmt, argptr);
   va_end(argptr);
-  return output;
+  return size;
 }
 #endif
