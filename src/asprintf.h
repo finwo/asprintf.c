18f1687dd34ab8bc9dba8c2f1f4822852241ea9b
Yersa Nordman <yersa@finwo.nl>
2023-10-25 22:59:12 +0200

    Actually make it properly alloc

--- src/asprintf.h
+++ src/asprintf.h
@@ -1,5 +1,5 @@
 #ifdef _GNU_SOURCE
 #include <stdio.h>
 #else
-char * asprintf(char **restrict strp, const char *restrict fmt, ...);
+int asprintf(char **restrict strp, const char *restrict fmt, ...);
 #endif
